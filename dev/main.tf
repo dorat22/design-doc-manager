d1d35be71880dd5584ba02d182b22d308b5f5f10
shmador <dorattar4@gmail.com>
2025-11-06 01:35:11 +0200

    fix: fixed misconfigurations

--- dev/main.tf
+++ dev/main.tf
@@ -2,8 +2,13 @@ provider "aws" {
   region = "us-east-1"
 }
 
-provider "aws" {
-  region = "us-east-1"
+terraform {
+  backend "s3" {
+    bucket  = ""
+    key     = ""
+    region  = ""
+    profile = ""
+  }
 }
 
 module "dev" {
